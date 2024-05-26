270ab9975dded5d70fa3122a00c8756d3c6d48f1
Joshua Bamidele Olayinka <110237255+Josh-bo@users.noreply.github.com>
2024-05-26 22:58:45 +0100

    Revert "Joshua"

--- utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-const catchAsync = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch((err) => {
-      console.log(err);
-      next(err);
-    });
-  };
-};
-module.exports = catchAsync;
