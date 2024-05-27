2a7dea83bc02f4844343c44d892f918167091438
Joshua Bamidele <olajoshua775@gmail.com>
2024-05-27 11:19:06 +0100

    Did some ammendment

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -10,6 +10,6 @@ router.get("/", landing);
 
 // POSTS
 router.post("/user/userLogin", userLogin);
-router.post("/user/register", registerUsers);
+router.post("/user/userRegister", registerUsers);
 
 module.exports = router;
