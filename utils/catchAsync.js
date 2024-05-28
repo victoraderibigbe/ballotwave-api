280439d6d48581e53e65b6fc4d2d0318de14eab7
Joshua Bamidele <olajoshua775@gmail.com>
2024-05-28 09:34:05 +0100

    I added middleware again, utils and vercel.json 2

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,49 +1,9 @@
-// const saySomething = (something) => {
-//   return something;
-// };
-
-// const catchAsync = () => {
-//   const say = "Hello";
-//   console.log(saySomething(say));
-// };
-
-// catchAsync();
-
-// const saySomething = (something, username) => {
-//   const obj = { by: username, message: something };
-//   return obj;
-// };
-// console.log(saySomething(undefined, "Joshabam").message);
-
-// const saySomething = (something, username) => {
-//   const obj = { by: username, message: something };
-//   return obj;
-// };
-// const triggerSaySomething = (something, username) => {
-//   // saySomething("hello world");
-//   return saySomething(something, username);
-// };
-
-// console.log(triggerSaySomething("Hey there", "John Doe"));
-
-// const controller = async (req, res, next) => {
-//   // Error("An error occured");
-//   // console.log(user)
-//   // throw {name: "JOSHMABError", message: "one more"}
-//   throw Error("An error occured");
-//   // jkjk
-//   // djdk
-//   // await UserActivation.find()
-// };
 const catchAsync = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((err) => {
       console.log(err);
-      next(err)
+      next(err);
     });
   };
 };
 module.exports = catchAsync;
-// catchAsync(async (req, res, next) => {
-// //   throw Error("JoshBam Error Occured");
-// })();
